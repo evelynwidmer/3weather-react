ebc6b055fa9c32794fbff3ca324acc06f1765721
evelynwidmer <evelyn4720@gmail.com>
2022-01-22 15:12:54 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Evelyn Widmer and is{" "}
-        <a
-          href="https://github.com/evelynwidmer/3weather-react"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Evelyn Widmer and is{" "}
+          <a
+            href="https://github.com/evelynwidmer/3weather-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
