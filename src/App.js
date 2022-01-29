8c40d23cdc875fa53fa9317a3da462437cbf6c55
evelynwidmer <evelyn4720@gmail.com>
2022-01-29 17:42:47 -0600

    Updates

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React from "react";
-import Weather from "./Weather";
-import "./App.css";
+import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
-
-        <footer>
-          This project was coded by Evelyn Widmer and is{" "}
-          <a
-            href="https://github.com/evelynwidmer/3weather-react"
-            target="_blank"
-          >
-            open-sourced on GitHub
-          </a>
-        </footer>
+        <Weather defaultCity="New York" />
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,93 @@
-import React from "react";
+import axios from "axios";
+import WeatherInfo from "./Weatherinfo";
+import WeatherForecast from "./WeatherForecast";
+import React, { useState } from "react";
 import "./Weather.css";
 
-export default function Weather() {
-  return <div className="Weather">Hello from Weather</div>;
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function search() {
+    const apiKey = "38cea223891d0c90e86cb4b3203b1b50";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function searchLocation(position) {
+    let apiKey = "38cea223891d0c90e86cb4b3203b1b50";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Type a city.."
+                autoFocus="on"
+                autoComplete="off"
+                className="form-control shadow-sm"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="form-control btn btn-primary shadow-sm w-100"
+              />
+            </div>
+            <div className="col-3">
+              <button
+                className="btn btn-success w-100"
+                onClick={getCurrentLocation}
+              >
+                Current
+              </button>
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
