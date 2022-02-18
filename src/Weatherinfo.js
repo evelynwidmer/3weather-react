fa34461928af88dce141670d2b06bcbbb6a3573e
evelynwidmer <evelyn4720@gmail.com>
2022-02-17 21:12:50 -0600

    Changed capital i

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherIcon";
+import WeatherIcon from "./Weathericon";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function Weatherinfo(props) {
